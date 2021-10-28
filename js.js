ad7aa46f2fb84773361ec627b88ab2733d8711f6
matiacuna <88457256+matiacuna@users.noreply.github.com>
2021-10-28 19:27:42 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
-let pv1 = ""
+let valpp = ""
+let valc1 = ""
+let valc2 = ""
+let valc3 = ""
+let valce = ""
+
+let pv1h = ""
+let pv1p = ""
 let pv2 = ""
 let pv3 = ""
 
@@ -8,22 +15,33 @@ let co3 = ""
 let co4 = ""
 let co5 = ""
 let co6 = ""
-let co7 = ""
-let co8 = ""
+
 
 let oc1 = ""
 let oc2 = ""
 let oc3 = ""
 let oc4 = ""
-let oc5 = ""
 
+
+let preVenta = ""
+let proyect = ""
+let proyectAndPreVenta = ""
 let util = ""
 let presupuesto = ""
 let cost = ""
 
-function calc(){
 
-    pv1 = parseInt(document.getElementById("pv1").value);
+//tomo los valores de horas y costos de horas
+function getValues(){
+
+    valpp = parseInt(document.getElementById("valpp").value);
+    valc1 = parseInt(document.getElementById("valc1").value);
+    valc2 = parseInt(document.getElementById("valc2").value);
+    valc3 = parseInt(document.getElementById("valc3").value);
+    valce = parseInt(document.getElementById("valce").value);
+
+    pv1h = parseInt(document.getElementById("pv1h").value);
+    pv1p = parseInt(document.getElementById("pv1p").value);
     pv2 = parseInt(document.getElementById("pv2").value);
     pv3 = parseInt(document.getElementById("pv3").value);
 
@@ -33,30 +51,134 @@ function calc(){
     co4 = parseInt(document.getElementById("co4").value);
     co5 = parseInt(document.getElementById("co5").value);
     co6 = parseInt(document.getElementById("co6").value);
-    co7 = parseInt(document.getElementById("co7").value);
-    co8 = parseInt(document.getElementById("co8").value);
+
 
     oc1 = parseInt(document.getElementById("oc1").value);
     oc2 = parseInt(document.getElementById("oc2").value);
     oc3 = parseInt(document.getElementById("oc3").value);
     oc4 = parseInt(document.getElementById("oc4").value);
-    oc5 = parseInt(document.getElementById("oc5").value);
 
-    cost = pv1 + pv2 + pv3 + co1 + co2 + co3 + co4 + co5 + co6 + co7 + co8 + oc1 + oc2 + oc3 + oc4 + oc5;
+}
+
+//calculo los valores de la preventa
+function calcPreVenta(){
+
+    preVenta = ((pv1h*pv1p) + pv2 + pv3)* valpp;
+
+}
+
+//calculo el valor del area proyecto segun la categoria seleccioanda
+function calcProyect(){
+
+    proyect = co1 + co2 + co3 + co4 + co5 + co6;
+
+    let catP = document.getElementById("cat").value;
+
+    if ( catP == "1" ){
+
+        proyect = proyect * valc1;
+    }
+    else if( catP == "2" ){
+        proyect = proyect * valc2;
+    }
+    else if( catP == "3" ){
+        proyect = proyect * valc3;
+    }
+    else if( catP == "4" ){
+        proyect = proyect * valce;
+    }
+
+
+}
+
+
+//calculo el valor de los otros costos estimados
+function calcOtrosCostos(){
+
+    proyectAndPreVenta = proyect + preVenta;
+    cost = proyectAndPreVenta;
+
+    let oct1 = document.getElementById("oct1").value;
+    let oct2 = document.getElementById("oct2").value;
+    let oct3 = document.getElementById("oct3").value;
+    let oct4 = document.getElementById("oct4").value;
+
+
+    if ( oct1 == 1){
+
+        oc1 = proyectAndPreVenta*(oc1 / 100);
+        cost = oc1 + cost;
+
+        
+    }else{
+        cost = oc1 + cost;
+        
+    };
+
+    if ( oct2 == 1){
+
+        oc2 = proyectAndPreVenta*(oc2 / 100);
+        cost = oc2 + cost;
+    }else{
+        cost = oc2 + cost;
+    };
+
+    if ( oct3 == 1){
+
+        oc3 = proyectAndPreVenta*(oc3 / 100);
+        cost = oc3 + cost;
+    }else{
+        cost = oc3 + cost;
+    };
+
+    if ( oct4 == 1){
+
+        oc4 = proyectAndPreVenta*(oc4 / 100);
+        cost = oc4 + cost;
+    }else{
+        cost = oc4 + cost;
+    };
+
+
+
+
+}
+
+
+//calculo el presupuesto, la utilidad los imprimo
+function calcTotal(){
+
+
+
     util = cost * 0.2;
     presupuesto = cost + util;
 
-    document.getElementById("porcentaje").innerHTML = `<h4> 20% del Costo: $ ${util}</h4>
+    document.getElementById("porcentaje").innerHTML = `
+
+
+    <div class="card-header">
+              <h4>Utilidad</h4>
+            </div>
+            <div class="card-body" id="porcentaje">
+
+
+            <h4> 20% del Costo: $ ${util}</h4>
+
+            </div>
+
     `
-    document.getElementById("presupuesto").innerHTML = `<div class="card text-white bg-primary mb-3 text-center border-primary">
+    document.getElementById("presupuesto").innerHTML = `<div class="card text-white bg-primary mb-3 text-center border-success">
     <div class="card-header ">
       <h4>Presupuesto</h4>
     </div>
     <div class="card-body" ><h4> $ ${presupuesto}</h4> </div>`
 
 
-
-
+    document.getElementById("costo").innerHTML = `<div class="card text-white bg-primary mb-3 text-center border-success">
+    <div class="card-header ">
+      <h4>Costo</h4>
+    </div>
+    <div class="card-body" ><h4> $ ${cost}</h4> </div>`
 
 
 }
@@ -70,7 +192,12 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 
     document.getElementById("boton").addEventListener("click", function(e){
-        calc();
+        getValues();
+        calcPreVenta();
+        calcProyect();
+        calcOtrosCostos();
+        calcTotal();
+        
     });
 
 
